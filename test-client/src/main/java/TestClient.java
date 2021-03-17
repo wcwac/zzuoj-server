73d86a6d2181e95ee5b95ee946c9eb44b56fa371
yuliuyuan <yuliuyuan@bytedance.com>
2021-03-17 17:58:05 +0800

    feat: 接下来要使用consul了
    Change-Id: I6b1384b922ad2c94770e134fa7a1b83b0e526303

--- test-client/src/main/java/TestClient.java
+++ test-client/src/main/java/TestClient.java
@@ -1,15 +1,10 @@
-package cn.edu.zzu.oj;
-
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
-public class AuthApplication {
+public class TestClient {
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+
     }
 }
--- zzuoj-auth/src/main/java/cn/edu/zzu/oj/controller/TestController.java
+++ zzuoj-auth/src/main/java/cn/edu/zzu/oj/controller/TestController.java
@@ -1,8 +1,10 @@
 package cn.edu.zzu.oj.controller;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +27,17 @@ public class TestController {
                 "port: "+port;
     }
 
-    @GetMapping("/test")
-    public String hello(){
-        return "hello salix";
+    @HystrixCommand(fallbackMethod = "hello1")
+    @GetMapping("/test/{id}")
+    public String hello(@PathVariable("id") String name){
+        if(!name.equals("salix")){
+            throw new RuntimeException();
+        }
+        return "hello "+name;
     }
+
+    public String hello01(@PathVariable("id") String name){
+        return "hello error";
+    }
+
 }
